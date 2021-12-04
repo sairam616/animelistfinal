cffd06912b00dde26d46d633af02f848a18dca54
SairamAvala <sairamavula99@gmail.com>
2021-12-03 21:51:46 -0500

    Initial Commit

---
+++ AnimeList_Final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeList_Final
+//
+//  Created by SaiRam Avala on 2021-12-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
